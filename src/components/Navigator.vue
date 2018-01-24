e0e90a1288bad9195f9d2b755b06278296021278
fmn <cdnxyf@gmail.com>
2018-01-24 22:06:43 +0800

    修改

--- src/components/Navigator.vue
+++ src/components/Navigator.vue
@@ -6,6 +6,18 @@
             <div class="logo">
               <img src='../assets/navigator/logo-50.png' height="50px" width="50px" />
             </div>
+            <div class="right_navigator">
+              <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+                <el-menu-item index="1">处理中心</el-menu-item>
+                <el-submenu index="2">
+                  <template slot="title">我的工作台</template>
+                  <el-menu-item index="2-1">选项1</el-menu-item>
+                  <el-menu-item index="2-2">选项2</el-menu-item>
+                  <el-menu-item index="2-3">选项3</el-menu-item>
+                </el-submenu>
+                <el-menu-item index="3"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
+              </el-menu>
+            </div>
         </el-col>
         <el-col :xs="0" :sm="3" :md="3" :lg="3" class="sideCol"></el-col>
       </el-row>
@@ -13,7 +25,7 @@
 </template>
 
 <script lang="ts">
-import { Button, Row, Col } from 'element-ui';
+import { Button, Row, Col, Menu, MenuItem, Submenu } from 'element-ui';
 import Vue, {ComponentOptions} from 'vue';
 export default Vue.extend({
   name: 'HelloWorld',
@@ -21,12 +33,22 @@ export default Vue.extend({
     'el-button':Button,
     'el-row':Row,
     'el-col':Col,
+    'el-menu':Menu,
+    'el-menu-item':MenuItem,
+    'el-submenu':Submenu,
   },
   data () {
     return {
-      msg: 'hellow ts'
+      msg: 'hellow ts',
+      activeIndex: '1',
+      activeIndex2: '1'
     }
-  }
+  },
+  methods: {
+      handleSelect(key:string, keyPath:string) {
+        console.log(key, keyPath);
+      }
+    }
 })
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
